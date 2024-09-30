d7f0d5fc5f63ec9b42ec44848af56afa38a94658
winebarrel <sugawara@winebarrel.jp>
2024-09-30 22:41:16 +0900

    first commit

---
+++ PagerCall/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PagerCall
+//
+//  Created by 菅原元気 on 2024/09/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PagerCall/PagerCallApp.swift
@@ -0,0 +1,17 @@
+//
+//  PagerCallApp.swift
+//  PagerCall
+//
+//  Created by 菅原元気 on 2024/09/30.
+//
+
+import SwiftUI
+
+@main
+struct PagerCallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
