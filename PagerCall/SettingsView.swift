08bacc595a4569eaead9a3f2d0878f971fc14583
winebarrel <sugawara@winebarrel.jp>
2024-10-20 14:08:26 +0900

    Add SettingView

---
+++ PagerCall/SettingsView.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Form {
+            // TODO:
+            Button {} label: {
+                Image(systemName: "leaf")
+            }
+        }
+    }
+}
